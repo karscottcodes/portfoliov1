383315674a9d92a614ce4090c87df87b74c000a1
Kyle S <kylearscott@gmail.com>
2024-05-27 10:06:06 -0400

    Deploy Update

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ include ('includes/database.php');
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>KARScott Codes | portfolio & resume</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/styles.css">
 </head>
 
